6e56a7f07d32e5fb6ed604f211263be546e04298
14KGun <geon6757@kaist.ac.kr>
2021-05-28 15:16:26 +0900

    Update PageNotFound.jsx

--- src/core/PageNotFound/PageNotFound.jsx
+++ src/core/PageNotFound/PageNotFound.jsx
@@ -5,26 +5,37 @@ import Footer from '../Frame/Footer'
 import svgPNF from './svg_pagenotfound.svg';
 
 const BtnBack = (props) => {
+    const [isHover, setHover] = useState(false);
+    const background = useSpring({
+        background: `rgba(230,230,230,${ isHover ? 1 : 0 })`,
+        config: { duration: 150 }
+    });
     const style = {
         display: 'inline-block', height: '42px', lineHeight: '42px', marginRight: '10px',
-        paddingLeft: '25px', paddingRight: '25px', borderRadius: '23px', border: '2px solid black',
-        color: 'black', fontSize: '16px', fontWeight: '300',
+        paddingLeft: '25px', paddingRight: '25px', borderRadius: '23px', border: '2px solid rgb(120,120,120)',
+        color: 'rgb(80,80,80)', fontSize: '16px', fontWeight: '300',
     };
     const onClickEvent = () => { window.history.back() };
     return (
-        <span onClick={ onClickEvent } className="BTNC" style={{ ...style }}>이전 페이지</span>
+        <animated.span onClick={ onClickEvent } onMouseEnter={ () => setHover(true) } onMouseLeave={ () => setHover(false) }
+        className="BTNC" style={{ ...style, ...background }}>이전 페이지</animated.span>
     )
 }
 const BtnHome = (props) => {
+    const [isHover, setHover] = useState(false);
+    const background = useSpring({
+        background: isHover ? 'rgb(0,120,170)' : 'rgb(0,134,191)',
+        config: { duration: 150 }
+    });
     const style = {
         display: 'inline-block', height: '46px', lineHeight: '46px',
         paddingLeft: '25px', paddingRight: '25px', borderRadius: '23px',
-        color: 'white', fontSize: '16px', fontWeight: '300',
-        background: 'black'
+        color: 'white', fontSize: '16px', fontWeight: '300'
     };
     return (
         <Link to="/">
-            <span style={{ ...style }}>메인으로</span>
+            <animated.span onMouseEnter={ () => setHover(true) } onMouseLeave={ () => setHover(false) }
+            style={{ ...style, ...background }}>메인으로</animated.span>
         </Link>
     )
 }
@@ -32,9 +43,9 @@ class PageNotFound extends Component {
     constructor(props){
         super(props);
         this.styleImg = { maxWidth: '500px', marginBottom: '10px' }
-        this.styleTxt1 = { fontSize: '60px', fontWeight: '900', lineHeight: '50px' }
-        this.styleTxt2 = { fontSize: '22px', fontWeight: '600', marginBottom: '10px' }
-        this.styleTxt3 = { fontSize: '16px', marginBottom: '30px' }
+        this.styleTxt1 = { fontSize: '60px', fontWeight: '900', lineHeight: '50px', color: 'rgb(52,59,71)' }
+        this.styleTxt2 = { fontSize: '22px', fontWeight: '600', marginBottom: '10px', color: 'rgb(52,59,71)' }
+        this.styleTxt3 = { fontSize: '16px', fontWeight: '300', marginBottom: '30px', color: 'rgb(129,139,156)' }
     }
     render() {
         return (
